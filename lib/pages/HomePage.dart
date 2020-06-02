9f1c58ccddf6820855cedc19e6453e4d19ea1695
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-06-01 21:13:59 -0400

    Firebase setup / Design Started

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,13 @@
+import 'package:buddiesgram/pages/NotificationsPage.dart';
+import 'package:buddiesgram/pages/ProfilePage.dart';
+import 'package:buddiesgram/pages/SearchPage.dart';
+import 'package:buddiesgram/pages/TimeLinePage.dart';
+import 'package:buddiesgram/pages/UploadPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn gSignIn = GoogleSignIn();
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,8 +15,142 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  bool isSignedIn = false;
+  PageController pageController;
+  int getPageIndex = 0;
+
+  void initState(){
+    super.initState();
+
+    pageController = PageController();
+
+    gSignIn.onCurrentUserChanged.listen((gSignInAccount) {
+      controlSignIn(gSignInAccount);
+    }, onError: (gError){
+      print('Error $gError');
+    });
+
+    gSignIn.signInSilently(suppressErrors: false).then((gSignInAccount){
+      controlSignIn(gSignInAccount);
+    }, onError: (gError){
+      print('Error $gError');
+    });
+  }
+
+  controlSignIn(GoogleSignInAccount signInAccount){
+    if(signInAccount != null)
+    {
+      setState(() {
+        isSignedIn = true;
+      });
+    }
+    else
+    {
+      setState(() {
+        isSignedIn = false;
+      });
+    }
+  }
+
+  void dispose(){
+    pageController.dispose();
+    super.dispose();
+  }
+
+  loginUser(){
+    gSignIn.signIn();
+  }
+
+  logoutUser(){
+    gSignIn.signOut();
+  }
+
+  whenPageChanges(int index){
+    this.getPageIndex = index;
+  }
+
+  onTapChangePage(int index){
+    pageController.animateToPage(index, duration: Duration(milliseconds: 400), curve: Curves.bounceInOut);
+  }
+
+  Widget buildHomeScreen(){
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          TimeLinePage(),
+          SearchPage(),
+          UploadPage(),
+          NotificationsPage(),
+          ProfilePage()
+        ],
+        controller: pageController,
+        onPageChanged: whenPageChanges,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: getPageIndex,
+        onTap: onTapChangePage,
+        activeColor: Colors.white,
+        inactiveColor: Colors.blueGrey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home)),
+          BottomNavigationBarItem(icon: Icon(Icons.search)),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle)),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications)),
+          BottomNavigationBarItem(icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+
+  Scaffold buildSignInScreen(){
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Theme.of(context).accentColor, Theme.of(context).primaryColor]
+          )
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'KikArt',
+              style: TextStyle(fontSize: 92.0, color: Colors.white, fontFamily: "Signatra"),
+            ),
+            GestureDetector(
+              onTap: () => loginUser(),
+              child: Container(
+                width: 270.0,
+                height: 65.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/google_signin_button.png"),
+                    fit: BoxFit.cover
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Here goes Home Page");
+    if(isSignedIn)
+    {
+      return buildHomeScreen();
+    }
+    else
+    {
+      return buildSignInScreen();
+    }
   }
 }
