6e31d51da0a4f29fe31af7ff82a05132e5c02e98
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-06-10 17:52:45 -0400

    Capture Image With Camera implemented

--- lib/pages/UploadPage.dart
+++ lib/pages/UploadPage.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPage extends StatefulWidget {
   @override
@@ -6,8 +8,60 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  File file;
+
+  captureImageWithCamera() async{
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 680,
+      maxWidth: 970
+    );
+    setState(() {
+      this.file = imageFile;
+    });
+  }
+
+  takeImage(mContext){
+    return showDialog(
+      context: mContext,
+      builder: (context){
+        return SimpleDialog(
+          title: Text("New Post", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          children: <Widget>[
+            SimpleDialogOption(
+              child: Text("Capture Image with Camera", style: TextStyle(color: Colors.white)),
+              onPressed: captureImageWithCamera,
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  displayUploadScreen(){
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.add_photo_alternate, color: Colors.grey, size: 200.0),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0)),
+              child: Text("Upload Image", style: TextStyle(color: Colors.white, fontSize: 20.0)),
+              color: Colors.green,
+              onPressed: () => takeImage(context),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Here goes Upload Page.");
+    return displayUploadScreen();
   }
 }
