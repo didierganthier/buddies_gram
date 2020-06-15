6ac56f525c6df381de2b179370c55b9975d73a3c
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-06-14 22:54:23 -0400

    EditProfilePage.dart completed

--- lib/pages/EditProfilePage.dart
+++ lib/pages/EditProfilePage.dart
@@ -1,3 +1,8 @@
+import 'package:buddiesgram/models/user.dart';
+import 'package:buddiesgram/pages/HomePage.dart';
+import 'package:buddiesgram/widgets/ProgressWidget.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
 
 class EditProfilePage extends StatefulWidget {
@@ -10,8 +15,180 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  TextEditingController profileNameTextEditingController = TextEditingController();
+  TextEditingController bioTextEditingController = TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool loading = false;
+  User user;
+  bool _bioValid = true;
+  bool _profileNameValid = true;
+
+  void initState(){
+    super.initState();
+
+    getAndDisplayUserInformation();
+  }
+
+  getAndDisplayUserInformation() async{
+    setState(() {
+      loading = true;
+    });
+
+    DocumentSnapshot documentSnapshot = await usersReference.document(widget.currentOnlineUserId).get();
+    user = User.fromDocument(documentSnapshot);
+
+    profileNameTextEditingController.text = user.profileName;
+    bioTextEditingController.text = user.bio;
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+  updateUserData(){
+    setState(() {
+      profileNameTextEditingController.text.trim().length < 3 || profileNameTextEditingController.text.isEmpty ? _profileNameValid = false : _profileNameValid = true;
+      bioTextEditingController.text.trim().length > 110 ? _bioValid = false : _bioValid = true;
+    });
+
+    if(_bioValid && _profileNameValid){
+      usersReference.document(widget.currentOnlineUserId).updateData({
+        "profileName": profileNameTextEditingController.text,
+        "bio": bioTextEditingController.text
+      });
+
+      SnackBar snackBar = SnackBar(content: Text("Profile has been updated"));
+      _scaffoldKey.currentState.showSnackBar(snackBar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Here goes Edit Profile Page');
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text("Edit Profile", style: TextStyle(color: Colors.white)),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.done, color: Colors.white, size: 30.0), onPressed: ()=> Navigator.pop(context))
+        ],
+      ),
+      body: loading? circularProgress() : ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 7.0),
+                  child: CircleAvatar(
+                    radius: 52.0,
+                    backgroundImage: CachedNetworkImageProvider(user.url),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(children: <Widget>[createProfileNameTextFormField(),createBioTextFormField()],),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 29.0, right: 50.0, left: 50.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      color: Colors.green,
+                      onPressed: updateUserData,
+                      child: Text(
+                        "Update",
+                        style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 29.0, right: 50.0, left: 50.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      color: Colors.red,
+                      onPressed: logoutUser,
+                      child: Text(
+                        "Logout",
+                        style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  logoutUser() async {
+    await gSignIn.signOut();
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
+  }
+
+  Column createProfileNameTextFormField(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 13.0),
+          child: Text(
+            "Profile Name", style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          style: TextStyle(color: Colors.white),
+          controller: profileNameTextEditingController,
+          decoration: InputDecoration(
+            hintText: "Write profile name here...",
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey)
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white)
+            ),
+            hintStyle: TextStyle(color: Colors.grey),
+            errorText: _profileNameValid? null: "Profile name is too short"
+          ),
+        )
+      ],
+    );
+  }
+
+  Column createBioTextFormField(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 13.0),
+          child: Text(
+            "Bio", style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          style: TextStyle(color: Colors.white),
+          controller: bioTextEditingController,
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          maxLength: 110,
+          decoration: InputDecoration(
+              hintText: "Write your bio here...",
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey)
+              ),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)
+              ),
+              hintStyle: TextStyle(color: Colors.grey),
+              errorText: _bioValid? null: "Bio is too long"
+          ),
+        )
+      ],
+    );
   }
 }
